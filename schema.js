61c3a02ebca7c87a80d6544298c7a66bc7692715
Andreas Ny <andreas.nyh87@gmail.com>
2020-09-04 12:40:33 +0200

    Get json-data with axios request

--- schema.js
+++ schema.js
@@ -41,7 +41,9 @@ const RootQuery = new GraphQLObjectType({
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        return customers;
+        return axios
+          .get('http://localhost:3000/customers')
+          .then((res) => res.data);
       },
     },
     customer: {
@@ -50,11 +52,9 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString },
       },
       resolve(parentValue, args) {
-        for (let i = 0; i < customers.length; i++) {
-          if (customers[i].id === args.id) {
-            return customers[i];
-          }
-        }
+        return axios
+          .get(`http://localhost:3000/customers/${args.id}`)
+          .then((res) => res.data);
       },
     },
   },
